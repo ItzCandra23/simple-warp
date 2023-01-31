cf85b7ffaf35e86c48f35f3fa78c76cf8ebe0df6
ItzCandra23 <95993220+ItzCandra23@users.noreply.github.com>
2023-02-01 00:17:35 +0700

    Delete src directory

--- src/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("./warps");
-require("./form");
-require("./commands");
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyJpbmRleC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOztBQUFBLG1CQUFpQjtBQUNqQixrQkFBZ0I7QUFDaEIsc0JBQW9CIn0=
--- src/index.ts
+++
@@ -1,3 +0,0 @@
-import "./warps";
-import "./form";
-import "./commands";
